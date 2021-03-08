b8f9158a0fffbfe3fbc2e1d205586cf52d747326
es5es5 <es5e5s@nate.com>
2021-03-08 11:33:00 +0900

    T-System

--- src/templates/tSystem/TSystem.vue
+++ src/templates/tSystem/TSystem.vue
@@ -20,7 +20,72 @@
         </ul>
       </div>
       <div class="contents_wrap">
-        contents_wrap
+        <ul class="menu_wrap">
+          <li class="menu-item active">상담요청</li>
+          <li class="menu-item">예약</li>
+          <li class="menu-item">고객</li>
+          <li class="menu-item">미승인DB</li>
+          <li class="menu-item">콜백</li>
+          <li class="menu-item">캠페인DB</li>
+        </ul>
+        <div class="table_wrap">
+          <table>
+            <colgroup>
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 50px;">
+              <col style="width: 100px;">
+            </colgroup>
+            <thead>
+              <tr>
+                <th scope="col"><span>No</span></th>
+                <th scope="col"><span>상담번호</span></th>
+                <th scope="col"><span>담당자</span></th>
+                <th scope="col"><span>고객명</span></th>
+                <th scope="col"><span>연락처</span></th>
+                <th scope="col"><span>유입경로</span></th>
+                <th scope="col"><span>상담일시</span></th>
+                <th scope="col"><span>메모</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><span>3</span></td>
+                <td><span>515212</span></td>
+                <td><span>빢끼훈</span></td>
+                <td><span>김루이</span></td>
+                <td><span>010-****-1111</span></td>
+                <td><span>지나가다가</span></td>
+                <td><span>2020-05-05</span></td>
+                <td><span>아무튼해줘</span></td>
+              </tr>
+              <tr>
+                <td><span>2</span></td>
+                <td><span>124124</span></td>
+                <td><span>양은찌</span></td>
+                <td><span>김루이</span></td>
+                <td><span>010-****-2222</span></td>
+                <td><span>심심해서</span></td>
+                <td><span>2020-10-15</span></td>
+                <td><span>간단한CRUD</span></td>
+              </tr>
+              <tr>
+                <td><span>1</span></td>
+                <td><span>424124</span></td>
+                <td><span>써씅원</span></td>
+                <td><span>김루이</span></td>
+                <td><span>010-****-3333</span></td>
+                <td><span>졸려서</span></td>
+                <td><span>2021-01-15</span></td>
+                <td><span>2시간이면하지?</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   </div>
@@ -81,5 +146,55 @@ main {
 .contents_wrap {
   width: calc(100% - 60px);
   float: left;
+}
+
+.menu_wrap {
+  list-style-type: none;
+  border: 1px solid rgba(#c3c8cd, .5);
+
+  .menu-item {
+    cursor: pointer;
+    display: inline-block;
+    margin: 0 10px;
+    padding: 15px 10px;
+    border-bottom: 2px solid #c3c8cd;
+
+    &:hover {
+      color: $primary;
+      border-bottom: 2px solid $primary;
+    }
+
+    &.active {
+      color: $primary;
+      border-bottom: 2px solid $primary;
+    }
+  }
+}
+
+table {
+  margin-top: 20px;
+  border-top: 2px solid $secondary;
+  border-bottom: 1px solid #dcdcdc;
+  border-left: 0;
+  border-right: 0;
+  border-radius: 0;
+  width: 100%;
+
+  th {
+    border-radius: 0;
+    background: $primary;
+    padding: 4px;
+    height: 34px;
+    text-align: center;
+    color: $secondary;
+  }
+
+  td {
+    padding: 4px;
+    height: 34px;
+    text-align: center;
+    border-top: 1px solid rgba(34,36,38,.1);
+    border-left: 1px solid rgba(34,36,38,.1);
+  }
 }
 </style>
